8a3b31658815186d2cd0469a27f0e3725d005b23
JelenaPlanic <jelena.lela.planic@gmail>
2023-11-19 17:42:28 +0100

    setup project

---
+++ config/config.js
@@ -0,0 +1,15 @@
+const PORT = 3000;
+const HOST = "localhost";
+const USER = "root";
+const PASSWORD = "";
+const DATABASE = "task_list";
+
+
+module.exports =
+{
+    PORT,
+    HOST,
+    USER,
+    PASSWORD,
+    DATABASE
+};
